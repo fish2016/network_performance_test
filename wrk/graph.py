c1fc1c6f2d72930f6c83639f6e1947a367d9b896
Your Name <you@example.com>
2025-01-31 13:24:37 +0800

    feat: add delay graph

--- wrk/graph.py
+++ wrk/graph.py
@@ -9,12 +9,14 @@ directory = "./wrk"  # Replace with the actual directory path
 
 requests_sec = defaultdict(list)
 transfers_sec = defaultdict(list)
+delay_time = defaultdict(list)
 
 mean_requests = {}
 mean_transfers = {}
+mean_delay = {}
 
-
-def plot(kind='', title='', ylabel='', means=None):
+# desc代表默认降序
+def plot(kind='', title='', ylabel='', means=None, desc=True):
     # Sort the labels and requests_sec lists together based on the requests_sec values
     labels = []
     values = []
@@ -24,8 +26,9 @@ def plot(kind='', title='', ylabel='', means=None):
         labels.append(k)
         values.append(v)
 
+
     # sort the labels and value lists 
-    labels, values = zip(*sorted(zip(labels, values), key=lambda x: x[1], reverse=True))
+    labels, values = zip(*sorted(zip(labels, values), key=lambda x: x[1], reverse=desc))
 
     # Plot the graph
     plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
@@ -75,6 +78,17 @@ if __name__ == '__main__':
                             value *= 1024 * 1024
                         value /= 1024.0 * 1024
                         transfers_sec[label].append(value)
+                    match = re.search(r"Latency\s+([\d.]+)\s*(\S+)", line)
+                    if match:
+                        value = float(match.group(1))
+                        unit = match.group(2) 
+                        if unit == "s":
+                            value *= 1000*1000  
+                        elif unit == "ms":  
+                            value *= 1000            
+
+                            
+                        delay_time[label].append(value)
 
     # calculate means
     for k, v in requests_sec.items():
@@ -83,8 +97,14 @@ if __name__ == '__main__':
     for k, v in transfers_sec.items():
         mean_transfers[k] = statistics.mean(v)
 
+    for k, v in delay_time.items():
+        mean_delay[k] = statistics.mean(v)        
+
     # save the plots
     plot(kind='req_per_sec', title='Requests/sec Comparison',
          ylabel='requests/sec', means=mean_requests)
     plot(kind='xfer_per_sec', title='Transfer/sec Comparison',
          ylabel='transfer/sec [MB]', means=mean_transfers)
+
+    plot(kind='delay', title='Delay Comparison',
+         ylabel='delay [us]', means=mean_delay, desc=False)         
